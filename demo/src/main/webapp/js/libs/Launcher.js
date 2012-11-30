a5b3a922ccafdce52279c3301b29c14fbac68f68
Joakim Kemeny <joakim@kemeny.se>
2012-11-30 15:54:56 +0100

    First version of the demo application.

---
+++ demo/src/main/webapp/js/libs/Launcher.js
@@ -0,0 +1,40 @@
+requirejs.config({
+
+    baseUrl : "js",
+
+    paths : {
+        backbone : "libs/backbone/backbone",
+        jquery : "libs/jquery/jquery",
+        knockback : "libs/knockback/knockback",
+        knockout : "libs/knockout/knockout",
+        text : "libs/require/require.text",
+        underscore : "libs/underscore/underscore",
+
+        modules : "../modules"
+    },
+
+    shim : {
+        backbone : {
+            deps : ["underscore", "jquery"],
+            exports : "Backbone"
+        },
+        bootstrap : ["jquery"],
+        jquery : {
+            exports : "$"
+        },
+        knockback : {
+            deps : ["underscore", "backbone", "knockout"],
+            exports : "kb"
+        },
+        underscore : {
+            deps : ["jquery"],
+            exports : "_"
+        }
+    }
+});
+
+require([
+    "Factory"
+], function (Factory) {
+    Factory.start();
+});
